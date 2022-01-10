fad05e353e926081dc69440033ebe9641445cb34
JakeSaintG <jakestgermain@outlook.com>
2022-01-10 17:38:20 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+ReadMe/Instruction is WIP
---
+++ js/index.js
@@ -0,0 +1,161 @@
+// There's no javascript here that controls the page. Good on you for looking, though.
+
+/*=====================================Bonus: Let's talk about FizzBuzz.======================================*/ 
+/*
+The FizzBuzz problem is a classic test given in coding interviews. This is the task:
+
+Print integers 1 to 100.
+Howevever, print “Fizz” if an integer is divisible by 3, “Buzz” if an integer is divisible by 5, and “FizzBuzz” if an integer is divisible by both 3 and 5.
+
+The beauty of FizzBuzz is how many ways there are to solve it. Take a crack at it when you're ready and scroll down to line 151 to see my first solution for FizzBuzz.
+*/
+
+
+/*=====================================Try your own FizzBuzz Solution here!======================================*/ 
+
+
+
+/*=====================================Try your own FizzBuzz Solution here!======================================*/ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (let index = 1; index < 101; index++) {
+//     if (index % 15 == 0) {
+//         console.log("FizzBuzz");
+//     } else if (index % 3 == 0) {
+//         console.log("Fizz");
+//     } else if (index % 5 == 0) {
+//         console.log("Buzz");
+//     } else {
+//         console.log(index)
+//     }
+// }
